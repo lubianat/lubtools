3ab3bbc71d79de7b89c40bebf350229ef07b85d0
Tiago Lubiana <tiagolubiana@hotmail.com>
2018-08-31 17:40:45 -0300

    first commit

---
+++ R/biomart_reference.R
@@ -0,0 +1,9 @@
+#'
+#' Dataset containing unique ENSG to Gene Symbol for humans
+#' as queried from biomaRt on august/2018
+#'
+#' @docType data
+#'
+#' @usage data(biomart_august_2018.RData)
+#'
+#' @keywords datasets
