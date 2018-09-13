72f562214cbd9d4ac5f4bd8e67b82c7f20437dd7
Tiago Lubiana <tiagolubiana@hotmail.com>
2018-09-13 15:45:07 -0300

    ensp function

--- R/ensg_to_gene_symbol.R
+++ R/ensg_to_gene_symbol.R
@@ -12,6 +12,22 @@
 
 ensg_to_gene_symbol <- function(ensg_character_vector){
   data('biomart_august_2018')
-  ensg_table[ensg_table$ensembl_gene_id %in% ensg_character_vector,]$hgnc_symbol
+  genes[genes$ensembl_gene_id %in% ensg_character_vector,]$hgnc_symbol
 }
 
+# this script runs biomart to chanse ENSG to gene symbols
+# WRITTEN BY TIAGO lUBIANA 18/08/2018
+
+
+#' ensg_to_ensp Function
+#'
+#' This function allows you to Gets biomart table (august/2018)
+#'  to change ensgs to ensp
+#' @param ensg_character_vector A character vector of ensemble gene ids (such as 'ENSG00000134057')
+#' @export
+
+
+ensg_to_ensp <- function(ensg_character_vector){
+  data('biomart_august_2018')
+  genes[ensg_table$ensembl_gene_id %in% ensg_character_vector,]$ensemble_peptide_id
+}
