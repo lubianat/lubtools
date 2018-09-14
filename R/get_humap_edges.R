357a6e53da00bf82cbd9532e1a2af09ac3b79175
Tiago Lubiana <tiagolubiana@hotmail.com>
2018-09-14 09:54:06 -0300

    get_humap_edges function

---
+++ R/get_humap_edges.R
@@ -0,0 +1,21 @@
+#' get_humap_edges Function
+#'
+# queries humap database for edges containing the genes of interest
+# return dataframe of edges
+#' @param vector_of_gene_symbols A character vector of gene symbols
+#' @export
+
+get_humap_edges <- function(vector_of_gene_symbols) {
+  require(lubtools)
+  data(humap)
+
+  require(dplyr)
+
+  node_one <- as.vector(humap$V1)
+  node_two <- as.vector(humap$V6)
+
+
+  humap %>%
+    filter(node_one %in% vector_of_gene_symbols |
+             node_two %in% vector_of_gene_symbols)
+}
