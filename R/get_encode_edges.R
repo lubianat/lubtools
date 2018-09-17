4a63cccded4a3e90134802e2cc62c8e3da2b8044
Tiago Lubiana <tiagolubiana@hotmail.com>
2018-09-17 14:57:17 -0300

    Created get_encode_edges function

--- R/get_encode_edges.R
+++ R/get_encode_edges.R
@@ -6,52 +6,83 @@
 #' @param vector_of_gene_symbols A character vector of gene symbols
 #' @param type_of_regulation Either 'direct' or 'common_tf'
 #' @export
-get_encode_edges <- function(vector_of_gene_symbols, type_of_regulation) {
-  require(lubtools)
-  require(dplyr)
-  data(encode)
-  if (type_of_regulation == 'common_tf'){
-    edgelist <- data.frame(edge_1 = c('mock'), edge_2 = c('mock'))
-    edgelist <- edgelist[-1, ]
 
+get_encode_edges <-
+  function(vector_of_gene_symbols,
+           type_of_regulation) {
+    require(lubtools)
+    require(dplyr)
+    data(encode)
+    if (type_of_regulation == 'common_tf') {
+      edgelist <- data.frame(edge_1 = c('mock'), edge_2 = c('mock'))
+      edgelist <- edgelist[-1,]
+      upstream_tfs <- list()
 
-  }
+      for (gene in vector_of_gene_symbols) {
+        print(gene)
+        print(flag)
+        flag = 0
+        n_of_genes_found = 0
+        for (each_set in encode$genesets) {
+          flag <- flag + 1
+          if (runif(1) > 0.999) {
+            print(each_set)
+          }
+          if (gene %in% each_set) {
+            n_of_genes_found <- n_of_genes_found + 1
+            #      print('it is!')
+            #      print(encode$geneset.names[flag])
+            tf <- encode$geneset.names[flag]
+            hm <- c(upstream_tfs[[gene]], tf)
+            upstream_tfs[[gene]] <- hm
+          }
 
+        }
 
-  upstream_tfs <- list()
-
-  for (gene in vector_of_gene_symbols) {
-    upstream_tfs[[gene]] <- list()
-    print(gene)
-    flag = 0
-    n_of_genes_found = 0
-    for (each_set in encode$genesets){
-      flag <- flag + 1
-      if (gene %in% each_set){
-      n_of_genes_found <- n_of_genes_found +1
-      print('it is!')
-      print(encode$geneset.names[flag])
-      tf <- encode$geneset.names[flag]
-      upstream_tfs[gene][[n_of_genes_found]] <- tf
 
       }
 
-    }
+      # i have no idea why the list generated also included each of the geneset.names.
+      # but as I don't want them, I have removed 'em
+      upstream_tfs <- upstream_tfs[-c(1:flag)]
 
-
-  }
-
-  for (kinase_target_now in 1:(length(upstream_kinases)-1)) {
-    print(kinase_target_now)
-    for (kinase_target_to_compare in (kinase_target_now+1):length(upstream_kinases)) {
-      print(kinase_target_to_compare)
-      if (any(upstream_kinases[[kinase_target_now]] %in% upstream_kinases[[kinase_target_to_compare]])) {
-        new_edge <- data.frame(names(upstream_kinases[kinase_target_now]), names(upstream_kinases[kinase_target_to_compare]))
-        edgelist <-  rbind(edgelist,new_edge )
+      for (upstream_tf_now in 1:(length(upstream_tfs) - 1)) {
+        print(upstream_tf_now)
+        for (upstream_tf_to_compare in (upstream_tf_now + 1):length(upstream_tfs)) {
+          print('vai')
+          print('vendo')
+          print(upstream_tf_now)
+          print('em')
+          print(upstream_tf_to_compare)
+          if (sum(upstream_tfs[[upstream_tf_now]] %in% upstream_tfs[[upstream_tf_to_compare]]) >= 3) {
+            print('hit!')
+            new_edge <-
+              data.frame(names(upstream_tfs[upstream_tf_now]), names(upstream_tfs[upstream_tf_to_compare]))
+            edgelist <-  rbind(edgelist, new_edge)
+          }
+        }
       }
+      colnames(edgelist) <- c('node_1', 'node_2')
+      return(edgelist)
+    }
+
+    if (type_of_regulation == 'direct') {
+      edgelist <- data.frame(edge_1 = c('mock'), edge_2 = c('mock'))
+      edgelist <- edgelist[-1,]
+      tfs <-
+        encode$geneset.names[encode$geneset.names %in% vector_of_gene_symbols]
+      target_sets <-
+        encode$genesets[encode$geneset.names %in% vector_of_gene_symbols]
+      for (gene_tf_index in 1:length(tfs)) {
+        for (gene_target in vector_of_gene_symbols) {
+          if (gene_target %in% target_sets[[gene_tf_index]] & gene_target != ""){
+            new_edge <- data.frame(tfs[gene_tf_index], gene_target)
+            edgelist <-  rbind(edgelist, new_edge)
+          }
+
+        }
+      }
+
     }
   }
-  colnames(edgelist) <- c('node_1', 'node_2')
-  return(edgelist)
 
-}
