2818aa8f4244bdffbc6013a5a5e6adc8152791e9
Tiago Lubiana <tiagolubiana@hotmail.com>
2018-09-18 16:18:50 -0300

    update

--- R/get_candidates_from_network.R
+++ R/get_candidates_from_network.R
@@ -4,16 +4,20 @@
 # return dataframe of edges
 #' @param network_measure either 'strength' or 'eigen_centrality'
 #' @param top_candidates the portion of candidates that will be taken
+#' @param blacklist vector of genes to be removed from candidate list, presumably because once they were degs
+#' @param g a igraph
 #' @export
 
 get_candidates_from_network <- function(g , network_measure, top_candidates, blacklist){
   require(igraph)
+  require(tibble)
   if (network_measure == 'strength'){
     gene_candidates <- as.data.frame(strength(g))
   }
   if (network_measure == 'eigen_centrality'){
     gene_candidates <- as.data.frame(eigen_centrality(g))
   }
+  gene_candidates <- rownames_to_column(gene_candidates, var = 'gene')
   gene_candidates <- gene_candidates %>%
     filter(!(gene %in% blacklist))
 
