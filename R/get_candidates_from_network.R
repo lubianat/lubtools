2e37d75dfa1d45af81a8a624d8b2a076d9c74804
Tiago Lubiana <tiagolubiana@hotmail.com>
2018-09-30 13:56:01 -0300

    removing cutoff in candidate network genes

--- R/get_candidates_from_network.R
+++ R/get_candidates_from_network.R
@@ -3,12 +3,11 @@
 # queries humap database for edges containing the genes of interest
 # return dataframe of edges
 #' @param network_measure either 'strength' or 'eigen_centrality'
-#' @param top_candidates the portion of candidates that will be taken
 #' @param blacklist vector of genes to be removed from candidate list, presumably because once they were degs
 #' @param g a igraph
 #' @export
 
-get_candidates_from_network <- function(g , network_measure, top_candidates, blacklist){
+get_candidates_from_network <- function(g , network_measure,  blacklist){
   require(igraph)
   require(tibble)
   require(dplyr)
@@ -21,11 +20,8 @@ get_candidates_from_network <- function(g , network_measure, top_candidates, bla
   gene_candidates <- rownames_to_column(gene_candidates, var = 'gene')
   gene_candidates <- gene_candidates %>%
     filter(!(gene %in% blacklist))
- names(gene_candidates)[2] <- 'measure'
-  gene_candidates_top <- gene_candidates %>%
-    arrange(desc(measure)) %>%
-    head(top_candidates*length(gene_candidates))
-  return(gene_candidates_top)
+
+  return(gene_candidates)
 
 
 
